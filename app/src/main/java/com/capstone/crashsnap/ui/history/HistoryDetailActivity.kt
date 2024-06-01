d5d5e8ebfd6d2f62d6633c6319e93e18466bbdbb
Ahmad Shafwan Dzaky <ahmadshafwandzaky@gmail.com>
2024-06-01 21:05:48 +0800

    feat: add history activity

---
+++ app/src/main/java/com/capstone/crashsnap/ui/history/HistoryDetailActivity.kt
@@ -0,0 +1,12 @@
+package com.capstone.crashsnap.ui.history
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.capstone.crashsnap.R
+
+class HistoryDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_history_detail)
+    }
+}
---
+++ app/src/main/java/com/capstone/crashsnap/ui/history/HistoryListActivity.kt
@@ -0,0 +1,12 @@
+package com.capstone.crashsnap.ui.history
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.capstone.crashsnap.R
+
+class HistoryListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_history_list)
+    }
+}
