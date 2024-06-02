c36c5ca5f1bb4b56d7c5233a5a910aa0a6fe31df
Snasset <dheril.oct@gmail.com>
2024-06-02 15:06:37 +0700

    feat: add configurations

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("com.google.android.libraries.mapsplatform.secrets-gradle-plugin")
 }
 
 android {
@@ -35,12 +36,21 @@ android {
     }
     buildFeatures {
         viewBinding = true
+        buildConfig = true
     }
 }
 
 dependencies {
+
+    implementation("com.google.android.gms:play-services-maps:18.0.0")
+    implementation("com.google.android.gms:play-services-location:18.0.0")
+
+    implementation ("androidx.exifinterface:exifinterface:1.3.6")
+    implementation ("de.hdodenhof:circleimageview:3.1.0")
+
     implementation("androidx.core:core-splashscreen:1.0.0")
     implementation ("androidx.core:core-splashscreen:1.0.0-beta02")
+
     implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.12.0")
