cb5766542bc47177f42fdd284d8785f822f569fa
Snasset <dheril.oct@gmail.com>
2024-06-02 15:05:25 +0700

    feat: add basic navigations

--- app/src/main/java/com/capstone/crashsnap/ui/main/MainActivity.kt
+++ app/src/main/java/com/capstone/crashsnap/ui/main/MainActivity.kt
@@ -1,12 +1,64 @@
 package com.capstone.crashsnap.ui.main
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.capstone.crashsnap.R
+import com.capstone.crashsnap.databinding.ActivityMainBinding
+import com.capstone.crashsnap.ui.auth.SignupActivity
+import com.capstone.crashsnap.ui.history.HistoryListActivity
+import com.capstone.crashsnap.ui.maps.MapsActivity
+import com.capstone.crashsnap.ui.profile.ProfileActivity
+import com.capstone.crashsnap.ui.result.ResultActivity
+import com.google.android.material.bottomsheet.BottomSheetDialog
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.historyButton.setOnClickListener {
+            startActivity(Intent(this, HistoryListActivity::class.java))
+        }
+        binding.topAppBar.setOnMenuItemClickListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.profile -> {
+                    startActivity(Intent(this, ProfileActivity::class.java))
+                    true
+                }
+                else -> false
+            }
+        }
+        binding.fab.setOnClickListener {
+            showBottomSheet()
+        }
+
+        binding.repairShopButton.setOnClickListener {
+            startActivity(Intent(this, MapsActivity::class.java))
+        }
+
+    }
+
+    private fun showBottomSheet(){
+        val dialogView = layoutInflater.inflate(R.layout.bottomsheet,null)
+        val dialog = BottomSheetDialog(this)
+        val galleryGroup = dialogView.findViewById<View>(R.id.galleryGroup)
+        val cameraGroup =  dialogView.findViewById<View>(R.id.cameraGroup)
+        galleryGroup.setOnClickListener{
+            // Open gallery
+//            startGallery()
+            startActivity(Intent(this, ResultActivity::class.java))
+            dialog.dismiss()
+        }
+        cameraGroup.setOnClickListener{
+            // Open camera
+//            startCamera()
+            startActivity(Intent(this, ResultActivity::class.java))
+            dialog.dismiss()
+        }
+        dialog.setContentView(dialogView)
+        dialog.show()
     }
 }
--- app/src/main/java/com/capstone/crashsnap/ui/result/ResultActivity.kt
+++ app/src/main/java/com/capstone/crashsnap/ui/result/ResultActivity.kt
@@ -1,12 +1,26 @@
 package com.capstone.crashsnap.ui.result
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.capstone.crashsnap.R
+import com.capstone.crashsnap.databinding.ActivityResultBinding
+import com.capstone.crashsnap.ui.main.MainActivity
+import com.capstone.crashsnap.ui.profile.ProfileActivity
 
 class ResultActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityResultBinding
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityResultBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_result)
+        setContentView(binding.root)
+
+        binding.saveResultButton.setOnClickListener {
+            startActivity(Intent(this, MainActivity::class.java))
+        }
+
+        binding.topAppBar.setNavigationOnClickListener {
+            onBackPressedDispatcher.onBackPressed()
+        }
     }
 }
