4f4ea638023e0fb910836c6c3bd8826c4eb61a6b
Snasset <dheril.oct@gmail.com>
2024-06-15 18:55:59 +0700

    refactor: add preference

--- app/src/main/java/com/capstone/crashsnap/Injection.kt
+++ app/src/main/java/com/capstone/crashsnap/Injection.kt
@@ -2,13 +2,16 @@ package com.dicoding.picodiploma.loginwithanimation.di
 
 import android.content.Context
 import com.capstone.crashsnap.data.Repository
+import com.capstone.crashsnap.data.UserPreference
+import com.capstone.crashsnap.data.dataStore
 import com.capstone.crashsnap.data.remote.retrofit.ApiConfig
+import kotlinx.coroutines.runBlocking
 
 object Injection {
     fun provideRepository(context: Context): Repository {
-//        val pref = UserPreference.getInstance(context.dataStore)
+        val pref = UserPreference.getInstance(context.dataStore)
         val apiService = ApiConfig.getApiService()
         val gmapsApiService = ApiConfig.getGooglePlacesService()
-        return Repository.getInstance(apiService,gmapsApiService )
+        return Repository.getInstance(pref, apiService, gmapsApiService)
     }
 }
