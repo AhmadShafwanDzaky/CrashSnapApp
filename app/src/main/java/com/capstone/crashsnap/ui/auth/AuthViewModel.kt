87fdc012a404316d7d4c4bddcc72e15f08bb5644
Snasset <dheril.oct@gmail.com>
2024-06-15 18:52:43 +0700

    feat: add login and session

--- app/src/main/java/com/capstone/crashsnap/ui/auth/AuthViewModel.kt
+++ app/src/main/java/com/capstone/crashsnap/ui/auth/AuthViewModel.kt
@@ -1,12 +1,26 @@
 package com.capstone.crashsnap.ui.auth
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
+import androidx.lifecycle.viewModelScope
 import com.capstone.crashsnap.data.Repository
-import okhttp3.RequestBody
+import com.capstone.crashsnap.data.UserModel
+import kotlinx.coroutines.launch
 
 class AuthViewModel(private val repository: Repository): ViewModel() {
 
     fun signup(name: String, email: String, password: String) =
         repository.signup(name, email, password)
+    fun login(email: String, password: String) = repository.login(email, password)
 
+    fun saveSession(token: String, name: String, email: String) {
+        viewModelScope.launch {
+            repository.saveSession(token, name, email)
+        }
+    }
+
+    fun getSession(): LiveData<UserModel> {
+        return repository.getSession().asLiveData()
+    }
 }
