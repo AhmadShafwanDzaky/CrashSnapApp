878a7a22cfc9a354efbca8c380ca8827aff779db
Snasset <dheril.oct@gmail.com>
2024-06-15 18:59:32 +0700

    feat: add more fetch API logic(login, logout, session, history)

--- app/src/main/java/com/capstone/crashsnap/data/Repository.kt
+++ app/src/main/java/com/capstone/crashsnap/data/Repository.kt
@@ -3,22 +3,150 @@ package com.capstone.crashsnap.data
 import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MediatorLiveData
+import androidx.lifecycle.asLiveData
+import com.capstone.crashsnap.data.remote.request.LoginRequest
 import com.capstone.crashsnap.data.remote.request.SignupRequest
+import com.capstone.crashsnap.data.remote.response.HistoryResponse
+import com.capstone.crashsnap.data.remote.response.LoginResponse
 import com.capstone.crashsnap.data.remote.response.NearbyPlaceResponse
 import com.capstone.crashsnap.data.remote.response.SignupResponse
 import com.capstone.crashsnap.data.remote.retrofit.ApiService
 import com.google.gson.Gson
+import com.google.gson.JsonSyntaxException
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.launch
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
 class Repository private constructor(
+    private val userPreference: UserPreference,
     private val apiService: ApiService,
     private val gmapsApiService: ApiService,
 ) {
 
     private val resultSignup = MediatorLiveData<NetResult<SignupResponse>>()
     private val resultNearbyPlace = MediatorLiveData<NetResult<NearbyPlaceResponse>>()
+    private val resultLogin = MediatorLiveData<NetResult<LoginResponse>>()
+    private val resultHistory = MediatorLiveData<NetResult<HistoryResponse>>()
+    private val resultAllHistory = MediatorLiveData<NetResult<HistoryResponse>>()
+
+
+    suspend fun saveSession(token: String, name: String, email: String) {
+        userPreference.saveSession(token, name, email)
+    }
+
+    fun getSession(): Flow<UserModel> {
+        return userPreference.getSession()
+    }
+
+    suspend fun logout() {
+        userPreference.logout()
+    }
+
+
+
+    fun getAllHistory(token: String): LiveData<NetResult<HistoryResponse>> {
+        resultAllHistory.value = NetResult.Loading
+        userPreference.getSession()
+        val bearerToken = "Bearer $token"
+        val client = apiService.getAllHistory(bearerToken)
+        client.enqueue(object : Callback<HistoryResponse> {
+            override fun onResponse(
+                call: Call<HistoryResponse>,
+                response: Response<HistoryResponse>
+            ) {
+                val res = response.body()
+                if (response.isSuccessful) {
+                    if (res != null) {
+                        resultAllHistory.value = NetResult.Success(res)
+                    }
+                } else {
+                    val errorResponse = response.errorBody()?.string()
+                    val errorBody = Gson().fromJson(errorResponse, LoginResponse::class.java)
+                    resultAllHistory.value = NetResult.Error(errorBody.message)
+                }
+            }
+
+            override fun onFailure(call: Call<HistoryResponse>, t: Throwable) {
+                resultAllHistory.value = NetResult.Error("fail ${t.message}")
+            }
+        })
+        return resultAllHistory
+    }
+
+    fun getHistory(token: String): LiveData<NetResult<HistoryResponse>> {
+        resultHistory.value = NetResult.Loading
+        userPreference.getSession()
+        val bearerToken = "Bearer $token"
+        val client = apiService.getHistory(bearerToken)
+        client.enqueue(object : Callback<HistoryResponse> {
+            override fun onResponse(
+                call: Call<HistoryResponse>,
+                response: Response<HistoryResponse>
+            ) {
+                val res = response.body()
+                if (response.isSuccessful) {
+                    if (res != null) {
+                        resultHistory.value = NetResult.Success(res)
+                    }
+                } else {
+                    val errorResponse = response.errorBody()?.string()
+                    val errorBody = Gson().fromJson(errorResponse, LoginResponse::class.java)
+                    resultHistory.value = NetResult.Error(errorBody.message)
+                }
+            }
+
+            override fun onFailure(call: Call<HistoryResponse>, t: Throwable) {
+                resultHistory.value = NetResult.Error("fail ${t.message}")
+            }
+        })
+        return resultHistory
+    }
+
+
+
+    fun login(
+        email: String,
+        password: String
+    ): LiveData<NetResult<LoginResponse>> {
+        resultLogin.value = NetResult.Loading
+
+        val loginRequest = LoginRequest(
+            email = email,
+            password = password
+        )
+
+        val client = apiService.login(loginRequest)
+        client.enqueue(object : Callback<LoginResponse> {
+            override fun onResponse(
+                call: Call<LoginResponse>,
+                response: Response<LoginResponse>
+            ) {
+                val res = response.body()
+                CoroutineScope(Dispatchers.Main).launch{
+                    if (response.isSuccessful) {
+                        if (res != null) {
+                            saveSession(res.loginResult.token, res.loginResult.displayName, email)
+                            resultLogin.value = NetResult.Success(res)
+                        }
+                    } else {
+                        val errorResponse = response.errorBody()?.string()
+                        val errorBody = Gson().fromJson(errorResponse, LoginResponse::class.java)
+                        resultLogin.value = NetResult.Error(errorBody.message)
+                    }
+                }
+            }
+
+            override fun onFailure(call: Call<LoginResponse>, t: Throwable) {
+                resultLogin.value = NetResult.Error("${t.message}")
+            }
+        })
+        return resultLogin
+    }
+
     fun signup(
         name: String,
         email: String,
@@ -41,13 +169,11 @@ class Repository private constructor(
                 val res = response.body()
                 if (response.isSuccessful) {
                     if (res != null) {
-                        Log.d("repo", "onResponse suk: nyampe")
                         resultSignup.value = NetResult.Success(res)
                     }
                 } else {
                     val errorResponse = response.errorBody()?.string()
                     val errorBody = Gson().fromJson(errorResponse, SignupResponse::class.java)
-                    Log.d("repo", "onResponse error: ${errorBody.message}")
                     resultSignup.value = NetResult.Error(errorBody.message)
                 }
             }
@@ -59,12 +185,15 @@ class Repository private constructor(
         return resultSignup
     }
 
+
+
+
     fun nearbyPlaces(
         type: String,
         location: String,
         radius: Int,
         apiKey: String
-    ): LiveData<NetResult<NearbyPlaceResponse>>{
+    ): LiveData<NetResult<NearbyPlaceResponse>> {
         resultNearbyPlace.value = NetResult.Loading
         val client = gmapsApiService.NearbyPlaces(type, location, radius, apiKey)
         client.enqueue(object : Callback<NearbyPlaceResponse> {
@@ -78,30 +207,28 @@ class Repository private constructor(
                         resultNearbyPlace.value = NetResult.Success(res)
                     }
                 } else {
-                    resultNearbyPlace.value = NetResult.Error("error ${res?.results}")
+                    resultNearbyPlace.value = NetResult.Error("Error ${res?.results}")
                 }
             }
 
             override fun onFailure(call: Call<NearbyPlaceResponse>, t: Throwable) {
-                resultNearbyPlace.value = NetResult.Error(" gksodo ${t.message}")
+                resultNearbyPlace.value = NetResult.Error(" Error ${t.message}")
             }
         })
         return resultNearbyPlace
     }
 
 
-
-
-
     companion object {
         @Volatile
         private var instance: Repository? = null
         fun getInstance(
+            userPreference: UserPreference,
             apiService: ApiService,
             gmapsApiService: ApiService,
         ): Repository =
             instance ?: synchronized(this) {
-                instance ?: Repository(apiService, gmapsApiService)
+                instance ?: Repository(userPreference, apiService, gmapsApiService)
             }.also { instance = it }
     }
 }
