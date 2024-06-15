79a0b65f8c3dfc63b7ee4770c756d95aeea9815a
Snasset <dheril.oct@gmail.com>
2024-06-15 18:58:13 +0700

    feat: preview image

--- app/src/main/java/com/capstone/crashsnap/ui/preview/PreviewActivity.kt
+++ app/src/main/java/com/capstone/crashsnap/ui/preview/PreviewActivity.kt
@@ -1,12 +1,22 @@
 package com.capstone.crashsnap.ui.preview
 
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.capstone.crashsnap.R
+import com.capstone.crashsnap.databinding.ActivityPreviewBinding
 
 class PreviewActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityPreviewBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_preview)
+        binding = ActivityPreviewBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.previewImageView.setImageURI(Uri.parse(intent.getStringExtra(EXTRA_IMAGE_STRING)))
+
+    }
+
+    companion object {
+        const val EXTRA_IMAGE_STRING = "extra_image_string"
     }
 }
