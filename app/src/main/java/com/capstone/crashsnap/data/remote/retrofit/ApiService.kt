0c3701969e329080a70c301e5529d519476d790f
Snasset <dheril.oct@gmail.com>
2024-06-15 18:52:08 +0700

    feat: add login and history, change maps query

--- app/src/main/java/com/capstone/crashsnap/data/remote/retrofit/ApiService.kt
+++ app/src/main/java/com/capstone/crashsnap/data/remote/retrofit/ApiService.kt
@@ -1,6 +1,9 @@
 package com.capstone.crashsnap.data.remote.retrofit
 
+import com.capstone.crashsnap.data.remote.request.LoginRequest
 import com.capstone.crashsnap.data.remote.request.SignupRequest
+import com.capstone.crashsnap.data.remote.response.HistoryResponse
+import com.capstone.crashsnap.data.remote.response.LoginResponse
 import com.capstone.crashsnap.data.remote.response.NearbyPlaceResponse
 import com.capstone.crashsnap.data.remote.response.SignupResponse
 import retrofit2.Call
@@ -13,9 +16,24 @@ interface ApiService {
         @Body request: SignupRequest
     ): Call<SignupResponse>
 
+    @POST("/api/auth/login")
+    fun login(
+        @Body request: LoginRequest
+    ): Call<LoginResponse>
+
+    @GET("/api/predictions")
+    fun getHistory(
+        @Header("Authorization") token: String,
+    ): Call<HistoryResponse>
+
+    @GET("/api/predictions")
+    fun getAllHistory(
+        @Header("Authorization") token: String,
+    ): Call<HistoryResponse>
+
     @GET("nearbysearch/json")
     fun NearbyPlaces(
-        @Query("type") type: String,
+        @Query("keyword") type: String,
         @Query("location") location: String,
         @Query("radius") radius: Int,
         @Query("key") apiKey: String
