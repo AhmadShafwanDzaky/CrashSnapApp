2009fe9b6d931f1ca9b6731932149ee3cab2bd1b
Ahmad Shafwan Dzaky <ahmadshafwandzaky@gmail.com>
2024-06-16 16:53:01 +0800

    refactor: add token to apiservice

--- app/src/main/java/com/capstone/crashsnap/Injection.kt
+++ app/src/main/java/com/capstone/crashsnap/Injection.kt
@@ -5,12 +5,14 @@ import com.capstone.crashsnap.data.Repository
 import com.capstone.crashsnap.data.UserPreference
 import com.capstone.crashsnap.data.dataStore
 import com.capstone.crashsnap.data.remote.retrofit.ApiConfig
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.runBlocking
 
 object Injection {
     fun provideRepository(context: Context): Repository {
         val pref = UserPreference.getInstance(context.dataStore)
-        val apiService = ApiConfig.getApiService()
+        val user = runBlocking { pref.getSession().first() }
+        val apiService = ApiConfig.getApiService(user.token)
         val gmapsApiService = ApiConfig.getGooglePlacesService()
         return Repository.getInstance(pref, apiService, gmapsApiService)
     }
