4207cd67b91892ae72b2c8e3f636a9ddd71f15a9
Snasset <dheril.oct@gmail.com>
2024-06-17 10:26:53 +0700

    feat: add tips and back navigation

--- app/src/main/java/com/capstone/crashsnap/ui/tips/TipsActivity.kt
+++ app/src/main/java/com/capstone/crashsnap/ui/tips/TipsActivity.kt
@@ -3,10 +3,18 @@ package com.capstone.crashsnap.ui.tips
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.capstone.crashsnap.R
+import com.capstone.crashsnap.databinding.ActivityResultBinding
+import com.capstone.crashsnap.databinding.ActivityTipsBinding
 
 class TipsActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityTipsBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_tips)
+        binding = ActivityTipsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.topAppBar.setNavigationOnClickListener {
+            onBackPressedDispatcher.onBackPressed()
+        }
     }
 }
